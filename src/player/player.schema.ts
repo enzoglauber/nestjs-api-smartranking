6ce66b891b4896f5aaf4b058d325ccb02784e236
Enzo Glauber <enzojob@gmail.com>
2023-05-25 20:40:19 -0300

    Player Schema

---
+++ src/player/player.schema.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export const PlayerSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String, unique: true
+    },
+    email: {
+      type: String, unique: true
+    },
+    name: String,
+    ranking: String,
+    position: Number,
+    photo: String
+  },
+  {
+    timestamps: true,
+    collection: 'players'
+  }
+)
