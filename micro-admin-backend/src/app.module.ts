3813cf8f8f95ac646ff165725a8a35738d86fb59
Enzo Glauber <enzojob@gmail.com>
2023-07-11 22:40:57 -0300

    app module

--- micro-admin-backend/src/app.module.ts
+++ micro-admin-backend/src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { CategorySchema } from './interfaces/category/category.schema'
+import { PlayerSchema } from './interfaces/player/player.schema'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true
+      }),
+      inject: [ConfigService]
+    }),
+    MongooseModule.forFeature([
+      { name: 'Player', schema: PlayerSchema },
+      { name: 'Category', schema: CategorySchema }
+    ])
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
