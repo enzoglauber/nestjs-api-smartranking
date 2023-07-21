90f5266d7dfa07ae90b748d95da3037ba2d5c3a4
Enzo Glauber <enzojob@gmail.com>
2023-07-21 17:16:07 -0300

    logging inteception

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,18 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import * as momentTimezone from 'moment-timezone';
-import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './shared/filters/http.exception.filter';
+import { NestFactory } from '@nestjs/core'
+import * as momentTimezone from 'moment-timezone'
+import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './shared/filters/http.exception.filter'
+import { LoggingInteceptor } from './shared/interceptions/logging.inteception'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter());
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalInterceptors(new LoggingInteceptor())
+  app.useGlobalFilters(new HttpExceptionFilter())
 
   Date.prototype.toJSON = (): string => {
-    return momentTimezone(this)
-      .tz('America/Sao_Paulo')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
-  };
+    return momentTimezone(this).tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS')
+  }
 
-  await app.listen(8080);
+  await app.listen(8080)
 }
-bootstrap();
+bootstrap()
---
+++ api-gateway/src/shared/interceptions/logging.inteception.ts
@@ -0,0 +1,16 @@
+import { CallHandler, Catch, ExecutionContext, Logger, NestInterceptor } from '@nestjs/common'
+import { Observable, tap } from 'rxjs'
+
+@Catch()
+export class LoggingInteceptor implements NestInterceptor {
+  private readonly logger = new Logger(LoggingInteceptor.name)
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>
+  ): Observable<any> | Promise<Observable<any>> {
+    const now = Date.now()
+    console.log(`before... `, now)
+    return next.handle().pipe(tap(() => console.log(`after...`, Date.now() - now)))
+  }
+}
