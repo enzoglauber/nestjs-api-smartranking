4c6287db8b8d1af3de4bb6c7d7cda2f7acd67f00
Enzo Glauber <enzojob@gmail.com>
2023-07-23 12:49:49 -0300

    fix eslint and prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    '@rocketseat/eslint-config/node',
+    '@rocketseat/eslint-config/node'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -22,12 +22,19 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
-  'prettier/prettier': [
-    'error',
-    {
-      singleQuote: true,
-      parser: 'flow',
-    },
-  ],
+    printWidth: 100,
+    tabWidth: 2,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'always',
+    semi: false,
+    endOfLine: 'auto'
+  }
+  // 'prettier/prettier': [
+  //   'error',
+  //   {
+  //     singleQuote: true,
+  //     parser: 'flow'
+  //   }
+  // ]
 }
