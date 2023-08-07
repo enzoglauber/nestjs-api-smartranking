6c3a41f18e8c6d3300c245ef9b6b9bd16f8f009e
Enzo Glauber <enzojob@gmail.com>
2023-08-07 19:07:42 -0300

    udpate category api gateway

--- api-gateway/src/category/category.controller.ts
+++ api-gateway/src/category/category.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Logger, Post, Query, UsePipes } from '@nestjs/common'
+import { Body, Controller, Get, Logger, Param, Post, Put, Query, UsePipes } from '@nestjs/common'
 import { ClientProxy } from '@nestjs/microservices'
 import { Observable, tap } from 'rxjs'
 import { ProxyRMQService } from 'src/proxyrmq/proxyrmq.service'
 import { ParamsValidationPipe } from 'src/shared/pipes/params-validation.pipe'
 import { Category } from './category.interface'
 import { InsertCategoryDto } from './dtos/insert-category.dto'
+import { UpdateCategoryDto } from './dtos/update-category.dto'
 @Controller('api/v1/category')
 export class CategoryController {
   private client: ClientProxy
@@ -24,5 +25,14 @@ export class CategoryController {
   all(@Query('id') id: string): Observable<Category[]> {
     this.logger.log(`category: ${JSON.stringify(id)}`)
     return this.client.send('all-categories', id ? id : '')
+  }
+
+  @Put('/:_id')
+  @UsePipes(ParamsValidationPipe)
+  update(@Body() category: UpdateCategoryDto, @Param('_id') _id: string) {
+    this.client.emit('update-category', {
+      id: _id,
+      category
+    })
   }
 }
