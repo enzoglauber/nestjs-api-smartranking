43e6656f8e8c8dba91950d6a3aee87b958d5a593
Enzo Glauber <enzojob@gmail.com>
2023-09-09 21:23:20 -0300

    fix injection

--- micro-rankings/src/ranking/ranking.module.ts
+++ micro-rankings/src/ranking/ranking.module.ts
@@ -3,11 +3,12 @@ import { RankingController } from './ranking.controller'
 import { RankingService } from './ranking.service'
 
 import { MongooseModule } from '@nestjs/mongoose'
+import { ProxyRMQService } from 'src/proxyrmq/proxyrmq.service'
 import { RankingSchema } from './ranking.schema'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }])],
-  providers: [RankingService],
+  providers: [RankingService, ProxyRMQService],
   controllers: [RankingController]
 })
 export class RankingModule {}
