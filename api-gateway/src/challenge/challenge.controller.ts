acd55087478b87a7eecaeca92b218b4a1ee4e7d9
Enzo Glauber <enzojob@gmail.com>
2023-09-19 08:35:04 -0300

    add challenge

--- api-gateway/src/challenge/challenge.controller.ts
+++ api-gateway/src/challenge/challenge.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -12,30 +11,18 @@ import {
   UsePipes,
   ValidationPipe
 } from '@nestjs/common'
-import { ClientProxy } from '@nestjs/microservices'
-import { lastValueFrom } from 'rxjs'
-import { Player } from 'src/player/player.interface'
-import { ProxyRMQService } from 'src/proxyrmq/proxyrmq.service'
 import { ChallengeService } from './challenge.service'
 import { AddChallengeDto } from './dtos/add-challenge.dto'
 import { AddMatchToChallenge } from './dtos/add-match-to-challenge.dto'
 import { UpdateChallengeDto } from './dtos/update-challenge.dto'
-import { ChallengeStatus } from './enum/challenge-status.enum'
 import { Challenge } from './interface/challenge.interface'
-import { Match } from './interface/match.interface'
 import { ChallengeStatusValidationPipe } from './pipes/challenge-status-validation.pipe'
 
 @Controller('api/v1/challengies')
 export class ChallengeController {
-  private admin: ClientProxy = this.proxyRMQService.get()
-  private challenge: ClientProxy = this.proxyRMQService.get('challenge')
-
   private logger = new Logger('micro-admin-backend')
 
-  constructor(
-    private readonly proxyRMQService: ProxyRMQService,
-    private readonly challengeService: ChallengeService
-  ) {}
+  constructor(private readonly challengeService: ChallengeService) {}
 
   @Post()
   @UsePipes(ValidationPipe)
@@ -44,59 +31,9 @@ export class ChallengeController {
     return await this.challengeService.add(challenge)
   }
 
-  private async validatePlayers(players: Player[], category: string): Promise<void> {
-    const playersResponse = await lastValueFrom(this.admin.send('all-player', ''))
-
-    for (const challengePlayer of players) {
-      const filter: Player[] = playersResponse.filter(
-        (player) => player._id === challengePlayer._id
-      )
-
-      if (filter.length === 0) {
-        throw new BadRequestException(`The id ${challengePlayer._id} isn't a player!`)
-      }
-
-      if (filter[0].category !== category) {
-        throw new BadRequestException(
-          `The player ${filter[0]._id} is not part of the specified category!`
-        )
-      }
-    }
-  }
-
-  private async validateRequester(players: Player[], requester: string): Promise<void> {
-    const isMatchRequester = players.some((player) => player._id === requester)
-
-    if (!isMatchRequester) {
-      throw new BadRequestException(`The requester has to be a player in the match`)
-    }
-  }
-
-  private async validateCategory(category: string): Promise<void> {
-    const categoryResponse = await lastValueFrom(this.admin.send('all-categories', category))
-
-    if (!categoryResponse) {
-      throw new BadRequestException(`Category does not exist!`)
-    }
-  }
-
   @Get()
   async all(@Query('playerId') playerId: string): Promise<Challenge | Challenge[]> {
-    if (playerId) {
-      const player: Player = await lastValueFrom(this.admin.send('all-player', playerId))
-      this.logger.log(`player: ${JSON.stringify(player)}`)
-      if (!player) {
-        throw new BadRequestException(`Player not found!`)
-      }
-    }
-
-    /*
-      In the challenges microservice, the method responsible for querying the challenges expect the structure below, where:
-      - If we fill in the playerId, the query of challenges will be by the id of the informed player
-      - If we fill in the _id field, the query will be by the challenge id
-      - If we do not fill in any of the two fields, the query will return all registered challenges
-    */
-    return lastValueFrom(this.challenge.send('all-challenges', { playerId, _id: '' }))
+    return this.challengeService.all(playerId)
   }
 
   @Put('/:id')
@@ -104,21 +41,7 @@ export class ChallengeController {
     @Body(ChallengeStatusValidationPipe) challenge: UpdateChallengeDto,
     @Param('id') _id: string
   ) {
-    const find: Challenge = await lastValueFrom(
-      this.challenge.send('all-challenges', { playerId: '', _id })
-    )
-
-    this.logger.log(`Challenge: ${JSON.stringify(find)}`)
-
-    if (!find) {
-      throw new BadRequestException(`Challenge not found!`)
-    }
-
-    if (find.status !== ChallengeStatus.PENDING) {
-      throw new BadRequestException('Only challenges with PENDING status can be updated!')
-    }
-
-    await this.challenge.emit('update-challenge', { _id, challenge })
+    await this.challengeService.update(_id, challenge)
   }
 
   @Post('/:id/match/')
@@ -126,55 +49,11 @@ export class ChallengeController {
     @Body(ValidationPipe) challenge: AddMatchToChallenge,
     @Param('id') _id: string
   ) {
-    const find: Challenge = await lastValueFrom(
-      this.challenge.send('all-challenges', { playerId: '', _id })
-    )
-
-    this.logger.log(`Challenge: ${JSON.stringify(find)}`)
-
-    if (!find) {
-      throw new BadRequestException(`Challenge not found!`)
-    }
-
-    if (find.status === ChallengeStatus.DONE) {
-      throw new BadRequestException(`Challenge already done!`)
-    }
-
-    if (find.status !== ChallengeStatus.ACCEPT) {
-      throw new BadRequestException(
-        `Matches can only be launched in challenges accepted by opponents!`
-      )
-    }
-
-    if (!find.players.includes(challenge.winner)) {
-      throw new BadRequestException(
-        `The winning player of the match must take part in the challenge!`
-      )
-    }
-
-    const match: Match = {
-      category: find.category,
-      winner: challenge.winner,
-      challenge: _id,
-      players: find.players,
-      result: challenge.result
-    }
-
-    await this.challenge.emit('add-match', match)
+    await this.challengeService.addChallengeMatch(_id, challenge)
   }
 
   @Delete('/:_id')
   async remove(@Param('_id') _id: string) {
-    const challenge: Challenge = await lastValueFrom(
-      this.challenge.send('all-challenges', { playerId: '', _id })
-    )
-
-    this.logger.log(`Challenge: ${JSON.stringify(challenge)}`)
-
-    if (!challenge) {
-      throw new BadRequestException(`Challenge not found!`)
-    }
-
-    await this.challenge.emit('remove-challenge', challenge)
+    this.challengeService.remove(_id)
   }
 }
--- api-gateway/src/challenge/challenge.service.ts
+++ api-gateway/src/challenge/challenge.service.ts
@@ -4,6 +4,11 @@ import { lastValueFrom } from 'rxjs'
 import { Player } from 'src/player/player.interface'
 import { ProxyRMQService } from 'src/proxyrmq/proxyrmq.service'
 import { AddChallengeDto } from './dtos/add-challenge.dto'
+import { AddMatchToChallenge } from './dtos/add-match-to-challenge.dto'
+import { UpdateChallengeDto } from './dtos/update-challenge.dto'
+import { ChallengeStatus } from './enum/challenge-status.enum'
+import { Challenge } from './interface/challenge.interface'
+import { Match } from './interface/match.interface'
 
 @Injectable()
 export class ChallengeService {
@@ -11,6 +16,86 @@ export class ChallengeService {
   private challengeRMQ: ClientProxy = this.proxyRMQService.get('challenges')
 
   constructor(private readonly proxyRMQService: ProxyRMQService) {}
+
+  async remove(_id: string): Promise<void> {
+    const challenge: Challenge = await lastValueFrom(
+      this.challengeRMQ.send('all-challenges', { playerId: '', _id })
+    )
+
+    if (!challenge) {
+      throw new BadRequestException(`Challenge not found!`)
+    }
+
+    await this.challengeRMQ.emit('remove-challenge', challenge)
+  }
+
+  async addChallengeMatch(_id: string, challenge: AddMatchToChallenge): Promise<void> {
+    const find: Challenge = await lastValueFrom(
+      this.challengeRMQ.send('all-challenges', { playerId: '', _id })
+    )
+
+    if (!find) {
+      throw new BadRequestException(`Challenge not found!`)
+    }
+
+    if (find.status === ChallengeStatus.DONE) {
+      throw new BadRequestException(`Challenge already done!`)
+    }
+
+    if (find.status !== ChallengeStatus.ACCEPT) {
+      throw new BadRequestException(
+        `Matches can only be launched in challenges accepted by opponents!`
+      )
+    }
+
+    if (!find.players.includes(challenge.winner)) {
+      throw new BadRequestException(
+        `The winning player of the match must take part in the challenge!`
+      )
+    }
+
+    const match: Match = {
+      category: find.category,
+      winner: challenge.winner,
+      challenge: _id,
+      players: find.players,
+      result: challenge.result
+    }
+
+    await this.challengeRMQ.emit('add-match', match)
+  }
+  async update(_id: string, challenge: UpdateChallengeDto): Promise<void> {
+    const find: Challenge = await lastValueFrom(
+      this.challengeRMQ.send('all-challenges', { playerId: '', _id })
+    )
+
+    if (!find) {
+      throw new BadRequestException(`Challenge not found!`)
+    }
+
+    if (find.status !== ChallengeStatus.PENDING) {
+      throw new BadRequestException('Only challenges with PENDING status can be updated!')
+    }
+
+    return await lastValueFrom(this.challengeRMQ.emit('update-challenge', { _id, challenge }))
+  }
+
+  async all<T = Challenge | Challenge[]>(playerId: string): Promise<T> {
+    if (playerId) {
+      const player: Player = await lastValueFrom(this.adminRMQ.send('all-player', playerId))
+      if (!player) {
+        throw new BadRequestException(`Player not found!`)
+      }
+    }
+
+    /*
+      In the challenges microservice, the method responsible for querying the challenges expect the structure below, where:
+      - If we fill in the playerId, the query of challenges will be by the id of the informed player
+      - If we fill in the _id field, the query will be by the challenge id
+      - If we do not fill in any of the two fields, the query will return all registered challenges
+    */
+    return lastValueFrom<T>(this.challengeRMQ.send<T>('all-challenges', { playerId, _id: '' }))
+  }
 
   async add(challenge: AddChallengeDto): Promise<void> {
     try {
