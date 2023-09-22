65198b77295e02386b46bcf57e21c3dcadea192a
Enzo Glauber <enzojob@gmail.com>
2023-09-22 09:17:59 -0300

    protection route with guard and jwt

--- api-gateway/src/player/player.controller.ts
+++ api-gateway/src/player/player.controller.ts
@@ -3,22 +3,28 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Post,
   Put,
   Query,
+  Req,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
   UsePipes
 } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 import { FileInterceptor } from '@nestjs/platform-express'
+import { Request } from 'express'
 import { ParamsValidationPipe } from 'src/shared/pipes/params-validation.pipe'
 import { SavePlayerDto } from './dtos/save-player.dto'
 import { Player } from './player.interface'
 import { PlayerService } from './player.service'
-
 @Controller('api/v1/players')
 export class PlayerController {
+  private logger = new Logger(PlayerController.name)
+
   constructor(private readonly playerService: PlayerService) {}
 
   @Post()
@@ -27,8 +33,10 @@ export class PlayerController {
     this.playerService.add(player)
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
-  async all(@Query('id') id: string): Promise<Player[]> {
+  async all(@Req() request: Request, @Query('id') id: string): Promise<Player[]> {
+    this.logger.log(`req: ${JSON.stringify(request.user)}`)
     return await this.playerService.all(id)
   }
 
